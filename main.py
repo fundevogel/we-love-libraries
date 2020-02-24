0be99312960a9eba65f3d7596286bfc486368f51
Martin Folkers <hello@twobrain.io>
2020-02-24 10:30:39 +0100

    Adding (more) meaningful comments

--- main.py
+++ main.py
@@ -41,30 +41,43 @@ if __name__ == "__main__":
     # Parsing arguments (if any)
     args = parser.parse_args()
 
-    # Loading URLs from JSON
-    base_urls = []
+    # Creating list for holding states & respective URLs
+    state_urls = []
 
     with open('states.json', 'r') as file:
+        # Load URLs from JSON
         data = json.load(file)
+
+        # Store them along with state names
         for state, url in data.items():
-            base_urls.append([state, url])
+            state_urls.append([state, url])
 
     # Fetching data, storing as JSON
-    for state, base_url in base_urls:
-        # (1) Check if states are provided
-        # (2)
+    for state, library_url in state_urls:
+        # (1) Check if any states are provided via CLI
+        # (2) Check if current state isn't among them
+        # (3) Move on to next state if both are true
         if len(args.states) is not False and state not in args.states:
             continue
 
+        # Build output path
         file = 'data/' + state + '.json'
 
-        if os.path.isfile(file) is False:
+        # Only (over)write files for states whose data ..
+        # (1) .. doesn't exist or
+        # (2) .. was aborted during execution
+        if os.path.isfile(file) is False or os.path.getsize(file) == 0:
             with open(file, 'w') as file:
-                library_urls = fetch(base_url)
+                # Fetch library URLs for all states
+                library_urls = fetch(library_url)
+
+                # Holds information of all libraries
                 libraries = []
 
+                # Parse each library URL, extract its data & save it
                 for library_url in library_urls:
                     library = parse(library_url)
                     libraries.append(library)
 
+                # Store data as (UTF-8 encoded) JSON file
                 json.dump(libraries, file, ensure_ascii=False, indent=4)
